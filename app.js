d20d00f738e79452a1016a88e86372ef9379a645
yerkezhank <77821505+yerkezhank@users.noreply.github.com>
2021-03-13 19:08:51 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 var http = require("http")
 var fs = require("fs")
 console.log(__dirname)
@@ -16,27 +15,46 @@ function serveStaticFile(res, path, contentType, responseCode) {
     })
 }
 
-
-http.createServer(function(req,res)
-{ var path = req.url.replace(/\/?(?:\?.*)?.*/,"").toLowerCase();
-switch(path)
-{case "":
- serveStaticFile(res,"/index.html", "text/html");
-break;
-case "/about":
-    serveStaticFile(res,"/about.html", "text/html");
+http.createServer(function(req, res){
+    var path = req.url.replace(/\/?(?:\?.*)?$/, "").toLowerCase();
+    switch(path){
+        case "":
+           serveStaticFile(res,"/index.html", "text/html");
+           break;
+        case "/about":
+            serveStaticFile(res,"/about.html", "text/html");
+           break;
+case "/img/cry.jpg":
+    serveStaticFile(res,"/img/cry.jpg", "image/jpeg");
+   break;
+case "/img/welcome.jpg":
+    serveStaticFile(res,"/img/welcome.jpg", "image/jpeg");
+   break;
+case "/img/about.jpg":
+    serveStaticFile(res,"/img/about.jpg", "image/jpeg");
+   break;
+case "/style.css":
+    serveStaticFile(res,"/style.css", "text/css");
+   break;
+case "/img/gallery/graduation.jpg":
+    serveStaticFile(res,"/img/gallery/graduation.jpg", "image/jpeg");
+   break;
+case "/img/gallery/study.jpg":
+    serveStaticFile(res,"/img/gallery/study.jpg", "image/jpeg");
+   break;
+case "/video/students/memes.mp4":
+    serveStaticFile(res, "/video/students/memes.mp4", "video/mp4");
     break;
-    case "/img/logo.jpg":
-        serveStaticFile(res,"/img/logo.jpg", "image/jpeg");
-        break;
-        default:
-            serveStaticFile(res, "/404.html","text/html",404 );
-            break;
+default:
+    serveStaticFile(res, "error.html", "text/html", 404);
+    break;
+
 }
-    
-    
-}). listen(3000)
+}).listen(3000)
 console.log("Serving is running on port 3000. Press CTRL+C to terminate");
+
+
+
 
 
 
